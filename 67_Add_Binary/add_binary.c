3cd73c101707b7636d984241c77e0db29a1850fd
vincent_chu <vincent_chu@asus.com>
2020-05-03 15:30:01 +0800

    67_Add_Binary

---
+++ 67_Add_Binary/add_binary.c
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>    
+
+using namespace std;
+
+string addBinary(string a, string b) {
+        int len_a = a.length();
+        int len_b = b.length();
+        string res("");
+        int carry = 0;
+        
+        int round = (len_a>=len_b) ? len_a : len_b;
+        int interval = (len_a>=len_b) ? len_a - len_b : len_b - len_a;
+        
+        while(interval--) {
+            if (len_a<len_b) {
+                a.insert(a.begin(), '0');
+            } else {
+                b.insert(b.begin(), '0');
+            }
+       }
+        //cout << "a=" << a << ", b=" << b << endl;
+        const char *str_a = a.c_str();
+        const char *str_b = b.c_str();
+        cout << "str_a=" << str_a << ", str_b=" <<  str_b << ", round=" << round << endl;
+        
+        for (int i=round-1; i>=0; i--) {
+                if ((str_a[i]-'0') != (str_b[i]-'0')) {  //0+1 or 1+0 case
+                    //cout << "Enter case 0+1 or 1+0" << endl;
+                    if (carry) {  //there is a carry in previous calculate
+                        res.insert(res.begin(), '0');
+                        carry = 1;
+                        continue;
+                    } else {
+                        res.insert(res.begin(), '1');
+                        carry = 0;
+                        continue;
+                    }
+                }
+            
+                if ((str_a[i]-'0') == 0 && (str_b[i]-'0') == 0) { //0+0 case
+                    //cout << "Enter case 0+0" << endl;
+                    if (carry) {
+                        res.insert(res.begin(), '1');
+                        carry = 0;
+                        continue;
+                    } else {
+                        res.insert(res.begin(), '0');
+                        carry = 0;
+                        continue;
+                    }
+                }
+            
+                if ((str_a[i]-'0') == 1 && (str_b[i]-'0') == 1) { //1+1 case
+                    //cout << "Enter case 1+1" << endl;
+                    if (carry) {
+                        res.insert(res.begin(), '1');
+                        carry = 1;
+                        continue;
+                    } else {
+                        res.insert(res.begin(), '0');
+                        carry = 1;
+                        continue;
+                    }
+                }
+        }
+        if (carry) //After last calculation there is still a carry
+            res.insert(res.begin(), '1');
+        return res;
+    }
+
+int main() {
+    string a("1010");
+    string b("1011");
+    string res;
+    res = addBinary(a, b);
+    cout << res << endl;
+}
